5fb133a91f9f97700e425ff4838f5d31a712c0be
caesarius00 <czarkowski15@gmail.com>
2022-11-30 23:16:51 +0100

    dark mode z obsługą sesji

--- ManagerFinansow/aplication/static/scripts.js
+++ ManagerFinansow/aplication/static/scripts.js
@@ -1,18 +1,38 @@
-function darkMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode-background");
-    if (element.classList.contains("dark-mode-background")) {
-        document.getElementById("site-content").style.backgroundColor = "#081132";
+function checkColorMode(){
+    if(sessionStorage.getItem("darkmode") == "true"){
+        var background = document.body;
         var button = document.getElementById("dark-mode-button");
+        var content = document.getElementById("site-content");
+        background.style.backgroundColor = "#2d428f";
+        background.style.color = "white";
+        content.style.backgroundColor = "#081132";
         button.style.backgroundColor = "darkblue";
         button.style.color = "white";
         button.innerHTML = "Jasny motyw";
-    } else {
-        document.getElementById("site-content").style.backgroundColor = "whitesmoke";
-        var button = document.getElementById("dark-mode-button");
-        button.style.backgroundColor = "white";
-        button.style.color = "black";
-        document.getElementById("dark-mode-button").innerHTML = "Ciemny motyw";
     }
 
+}
+
+function darkMode() {
+    var background = document.body;
+    var button = document.getElementById("dark-mode-button");
+    var content = document.getElementById("site-content");
+
+    if (sessionStorage.getItem("darkmode") == "false" || sessionStorage.getItem("darkmode") == null) {
+        background.style.backgroundColor = "#2d428f";
+        background.style.color = "white";
+        content.style.backgroundColor = "#081132";
+        button.style.backgroundColor = "darkblue";
+        button.style.color = "white";
+        button.innerHTML = "Jasny motyw";
+        sessionStorage.setItem("darkmode", "true");
+    } else {
+        background.style.backgroundColor = "white";
+        background.style.color = "black";
+        content.style.backgroundColor = "whitesmoke";
+        button.style.backgroundColor = "white";
+        button.style.color = "black";
+        button.innerHTML = "Ciemny motyw";
+        sessionStorage.setItem("darkmode", "false");
+    }
  }
