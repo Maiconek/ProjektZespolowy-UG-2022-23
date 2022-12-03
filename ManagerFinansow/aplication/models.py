32be43bffa5a07a398e1fba8f7000f8694a860de
Wojtek <umboj2000@gmail.com>
2022-12-03 15:15:27 +0100

    Zmiany ze spotkania biznesowego

--- ManagerFinansow/aplication/models.py
+++ ManagerFinansow/aplication/models.py
@@ -7,11 +7,13 @@ from UsersApp.models import *
 
 # Create your models here.
 
+# 1. kategorie podczepione do użytkownika (n-1), współdzielone dziedziczą po założycielu
 
 class Category(models.Model):
     name = models.CharField(max_length=255) 
     id = models.UUIDField(default=uuid.uuid4, unique=True, 
                             primary_key=True, editable=False)
+    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -65,7 +67,8 @@ class User_Account(models.Model):
 class Transaction(models.Model):
     id_account = models.ForeignKey(Account, on_delete=models.CASCADE)
     id_user = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True) #konto z którego dokonano transakcji zostało usunięte ale transakcja ma pozostać 
-    id_subcategory = models.ForeignKey(Subcategory, on_delete=models.SET_NULL, null=True) #umozliwienie usuniecia kategorii i podkategorii bez usuniecia transakcji
+    # kategoria
+    id_subcategory = models.ForeignKey(Subcategory, on_delete=models.SET_NULL, null=True, blank=True) #umozliwienie usuniecia kategorii i podkategorii bez usuniecia transakcji
     name = models.CharField(max_length=50)
     is_periodic = models.BooleanField(default=False)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
@@ -76,4 +79,4 @@ class Transaction(models.Model):
                             primary_key=True, editable=False)
 
     def __str__(self):
-        return "[{}] - {} - {} - {} - {} - {} - {} - {} - {} - {}".format(self.name, self.id_account.name, self.id_user.name, self.id_subcategory.id_category.name ,self.id_subcategory, self.is_periodic, self.amount, self.converted_amount, self.transaction_date, self.description)
+        return "[{}] - {} - {} - {} - {} - {} - {} - {} - {} - {}".format(self.name, self.id_account.name, self.id_user.name, self.id_subcategory, self.id_subcategory, self.is_periodic, self.amount, self.converted_amount, self.transaction_date, self.description)
