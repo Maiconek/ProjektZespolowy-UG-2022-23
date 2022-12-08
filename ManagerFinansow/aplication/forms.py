1a1f81d5a7970e414a508161461683014da6e205
michalm10 <m.marcinkowski.su@gmail.com>
2022-12-08 16:10:59 +0100

    merge

--- ManagerFinansow/aplication/forms.py
+++ ManagerFinansow/aplication/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.forms import ModelForm
-from .models import Account, Transaction
+from django.db import models
+from .models import Account, Transaction, User_Account
+from UsersApp.models import Category, Subcategory
+from django.db.models import Q
 
 
 class AccountForm(ModelForm):
@@ -12,3 +15,19 @@ class TransactionForm(ModelForm):
     class Meta:
         model = Transaction
         exclude = ['id_account', 'id_user', 'converted_amount', 'transaction_date']
+
+    def __init__(self, *, scope = "EXPENSE", Accountless = False, owner, **kwargs):
+        super(TransactionForm, self).__init__(**kwargs)
+        owned_categories = Category.objects.filter(Q(owner=owner) | Q(owner=None))
+        if Accountless:
+            self.fields['id_account'] = forms.ModelChoiceField(queryset = Account.objects.filter(id__in=User_Account.objects.
+                                                            filter(id_user=owner).values_list('id_account')), required=True)
+        if scope == "INCOME":
+            self.fields['id_category'].queryset = owned_categories.filter(scope=scope)
+        if scope == "EXPENSE":
+            self.fields['id_category'].queryset = owned_categories.filter(scope=scope)
+        self.fields['id_subcategory'].queryset = Subcategory.objects.filter(id_category__in=self.fields['id_category'].queryset)
+
+    def save(self, commit=True):
+        # do something with self.cleaned_data['temp_id']
+        return super(TransactionForm, self).save(commit=commit)
---
+++ ManagerFinansow/aplication/views/transaction_views.py
@@ -0,0 +1,125 @@
+from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
+from django.contrib.auth.decorators import login_required
+from aplication.models import *
+from aplication.forms import TransactionForm
+from datetime import datetime, date
+from django.http import Http404  
+
+@login_required(login_url='login')
+def addExpense(request, pk = None):
+    if pk is not None:
+        account = get_object_or_404(Account, id=pk)
+        if account.owner != request.user.profile:
+            raise Http404
+    form = TransactionForm(scope="EXPENSE", owner=request.user.profile, Accountless=pk is None, initial={'currency': request.user.profile.currency})
+    context = {'form': form, 'option': "add", 'today': date.today().strftime("%Y-%m-%d")}
+    if pk is not None:
+        context.update({'account': account})
+    else:
+        context.update({'account': None})
+
+    if request.method == "POST":
+        form = TransactionForm(data=request.POST or None, owner=request.user.profile)
+        if form.is_valid():
+            print(form.cleaned_data)
+            transaction = form.save(commit=False)
+            if pk is not None:
+                transaction.id_account=account
+            else:
+                transaction.id_account = form.cleaned_data['Account']
+            transaction.id_user = request.user.profile
+            transaction.transaction_date = datetime.now()
+            transaction.amount = -form.cleaned_data['amount']
+            transaction.converted_amount = -form.cleaned_data['amount']    
+            _date = request.POST['date']
+            transaction.transaction_date = _date   
+            transaction.save()
+            return redirect('account', pk=transaction.id_account.id)
+    return render(request, 'application/transaction/form.html', context)
+
+@login_required(login_url='login')
+def addIncome(request, pk):
+    account = get_object_or_404(Account, id=pk)
+    if account.owner != request.user.profile:
+        raise Http404
+    form = TransactionForm(scope = "INCOME", owner=request.user.profile, initial={'currency': account.currency})
+    context = {'account': account, 'form': form, 'option': "add", 'today': date.today().strftime("%Y-%m-%d")}
+
+    if request.method == "POST":
+        form = TransactionForm(data=request.POST or None, scope="INCOME", owner=request.user.profile)
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            transaction.id_account=account
+            transaction.id_user = request.user.profile
+            transaction.transaction_date = datetime.now()
+            transaction.converted_amount = form.cleaned_data['amount']     
+            _date = request.POST['date']
+            transaction.transaction_date = _date 
+            transaction.save()
+            return redirect('account', pk=account.id)
+    return render(request, 'application/transaction/form.html', context)
+
+@login_required(login_url='login')
+def duplicate(request, pk):
+    transaction = get_object_or_404(Transaction, id=pk)
+    if transaction.id_account.owner != request.user.profile:
+        raise Http404
+    transaction.id = None
+    form = TransactionForm(data=request.POST or None, scope = transaction.id_category.scope, 
+                            owner=transaction.id_account.owner, instance=transaction, 
+                            initial={'amount': -transaction.amount if transaction.id_category.scope=="EXPENSE" else transaction.amount})
+    if form.is_valid():
+        form.save(commit=False)
+        amnt = form.cleaned_data['amount']
+        transaction.amount = -amnt if transaction.id_category.scope=="EXPENSE" else amnt
+        transaction.converted_amount = -amnt if transaction.id_category.scope=="EXPENSE" else amnt
+        _date = request.POST['date']
+        transaction.transaction_date = _date 
+        transaction.save()
+        return  redirect('account', pk=transaction.id_account.id)
+    return render(request, 'application/transaction/form.html', {
+                                                                'form': form, 
+                                                                'account': transaction.id_account, 
+                                                                'option': "add", 
+                                                                'transaction': transaction,
+                                                                'today': date.today().strftime("%Y-%m-%d")})
+
+@login_required(login_url='login')
+def showTransaction(request, pk):
+    transaction = get_object_or_404(Transaction, id=pk)
+    if transaction.id_account.owner != request.user.profile:
+        raise Http404
+    context = {'tr':transaction}
+    return render(request, 'application/transaction/show.html', context)
+
+@login_required(login_url='login')
+def delTransaction(request, pk):
+    transaction = get_object_or_404(Transaction, id=pk)
+    if transaction.id_account.owner != request.user.profile:
+        raise Http404
+    account = transaction.id_account
+    transaction.delete()
+    return redirect('account', pk=account.id)
+
+@login_required(login_url='login')
+def editTransaction(request, pk):
+    transaction = get_object_or_404(Transaction, id=pk)
+    if transaction.id_account.owner != request.user.profile:
+        raise Http404
+    form = TransactionForm(data=request.POST or None, scope = transaction.id_category.scope, 
+                            owner=transaction.id_account.owner, instance = transaction,
+                            initial={'amount': -transaction.amount if transaction.id_category.scope=="EXPENSE" else transaction.amount})
+    if form.is_valid():
+        transaction = form.save(commit=False)
+        amnt = form.cleaned_data['amount']
+        transaction.amount = -amnt if transaction.id_category.scope=="EXPENSE" else amnt
+        transaction.converted_amount = -amnt if transaction.id_category.scope=="EXPENSE" else amnt
+        _date = request.POST['date']
+        transaction.transaction_date = _date
+        transaction.save()   
+        return redirect('showTransaction', pk=transaction.id)
+    return render(request, 'application/transaction/form.html', {
+                                                                'form': form, 
+                                                                'account': transaction.id_account, 
+                                                                'option': "edit", 
+                                                                'transaction': transaction})
