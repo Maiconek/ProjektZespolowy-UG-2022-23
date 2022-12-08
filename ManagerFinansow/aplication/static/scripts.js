6e0247574d60e4f1f2c82114ddb97f759ba76670
caesarius00 <czarkowski15@gmail.com>
2022-12-08 15:58:49 +0100

    Przeniesienie obsługi ciemnego motywu do jednej funkcji
    Teraz wystarczy zmienić jedynie w "changeToDark" aby ustawić jak cos ma wyglądać w ciemnym motywie

--- ManagerFinansow/aplication/static/scripts.js
+++ ManagerFinansow/aplication/static/scripts.js
@@ -1,63 +1,74 @@
+function changeToDark(){
+    var background = document.body;
+    var button = document.getElementById("dark-mode-button");
+    var content = document.getElementById("site-content");
+    var allAccounts = document.getElementById("all-accounts");
+    var allIncomes = document.getElementsByClassName("income");
+    var allExpenses = document.getElementsByClassName("expense");
+
+    var background = document.body;
+    var button = document.getElementById("dark-mode-button");
+    var content = document.getElementById("site-content");
+    var allAccounts = document.getElementById("all-accounts");
+    var allIncomes = document.getElementsByClassName("income");
+    var allExpenses = document.getElementsByClassName("expense");
+
+    //WOJTEK
+    var allTransactions = document.getElementsByClassName("transaction");
+    var linkButton = document.getElementsByClassName("button");
+
+    background.style.backgroundColor = "#2d428f";
+    background.style.color = "white";
+    content.style.backgroundColor = "#081132";
+    button.style.backgroundColor = "darkblue";
+    button.style.color = "white";
+    button.innerHTML = "Jasny motyw";
+
+    
+    //poszczególne banki (kolejne dzieci elementu all-accounts)
+    if(allAccounts != null){
+        for (var i = 0; i < allAccounts.children.length; i++) {
+            var description = allAccounts.children[i].querySelector(".account-description");
+            description.style.color = "rgba(255, 255, 255, 0.6)";
+            if(i%2 == 0){
+                allAccounts.children[i].style.backgroundColor = "rgb(12, 6, 35)";
+            }
+            else{
+                allAccounts.children[i].style.backgroundColor = "rgb(34, 15, 8)";
+            }
+            allAccounts.children[i].getElementsByClassName("account-link")[0].style.color = "white";
+        }
+    }
+
+    //WOJTEK
+    //transakcje
+    if(allTransactions != null){
+        for (var i = 0; i < allTransactions.length; i++) {
+            allTransactions[i].style.backgroundColor = "rgb(161, 161, 119)";
+        }
+    }
+    
+    //przychody
+    if(allIncomes != null){
+        for (var i = 0; i < allIncomes.length; i++) {
+            // allIncomes[i].style.backgroundColor = "rgb(37, 73, 37)";
+            allIncomes[i].style.color = "rgb(37, 73, 37)";
+        }
+    }
+
+    //wydatki
+    if(allExpenses != null){
+        for (var i = 0; i < allExpenses.length; i++) {
+            // allExpenses[i].style.backgroundColor = "rgb(73, 37, 37)";
+            allExpenses[i].style.color = "rgb(73, 37, 37)";
+        }
+        }
+
+}
+
 function checkColorMode(){
     if(sessionStorage.getItem("darkmode") == "true"){
-        var background = document.body;
-        var button = document.getElementById("dark-mode-button");
-        var content = document.getElementById("site-content");
-        var allAccounts = document.getElementById("all-accounts");
-        var allIncomes = document.getElementsByClassName("income");
-        var allExpenses = document.getElementsByClassName("expense");
-
-        //WOJTEK
-        var allTransactions = document.getElementsByClassName("transaction");
-        var linkButton = document.getElementsByClassName("button");
-
-        background.style.backgroundColor = "#2d428f";
-        background.style.color = "white";
-        content.style.backgroundColor = "#081132";
-        button.style.backgroundColor = "darkblue";
-        button.style.color = "white";
-        button.innerHTML = "Jasny motyw";
-
-        
-        //poszczególne banki (kolejne dzieci elementu all-accounts)
-        if(allAccounts != null){
-            for (var i = 0; i < allAccounts.children.length; i++) {
-                var description = allAccounts.children[i].querySelector(".account-description");
-                description.style.color = "rgba(255, 255, 255, 0.6)";
-                if(i%2 == 0){
-                    allAccounts.children[i].style.backgroundColor = "rgb(12, 6, 35)";
-                }
-                else{
-                    allAccounts.children[i].style.backgroundColor = "rgb(34, 15, 8)";
-                }
-                allAccounts.children[i].getElementsByClassName("account-link")[0].style.color = "white";
-            }
-        }
-
-        //WOJTEK
-        //transakcje
-        if(allTransactions != null){
-            for (var i = 0; i < allTransactions.length; i++) {
-                allTransactions[i].style.backgroundColor = "rgb(161, 161, 119)";
-            }
-        }
-        
-        //przychody
-        if(allIncomes != null){
-            for (var i = 0; i < allIncomes.length; i++) {
-                // allIncomes[i].style.backgroundColor = "rgb(37, 73, 37)";
-                allIncomes[i].style.color = "rgb(37, 73, 37)";
-            }
-        }
-
-        //wydatki
-        if(allExpenses != null){
-            for (var i = 0; i < allExpenses.length; i++) {
-                // allExpenses[i].style.backgroundColor = "rgb(73, 37, 37)";
-                allExpenses[i].style.color = "rgb(73, 37, 37)";
-            }
-        }
-
+        changeToDark();
     }
 
 }
@@ -76,56 +87,7 @@ function darkMode() {
 
     //ustawianie ciemnego motywu
     if (sessionStorage.getItem("darkmode") == "false" || sessionStorage.getItem("darkmode") == null) {
-        background.style.backgroundColor = "#2d428f";
-        background.style.color = "white";
-
-        //poszczególne banki (kolejne dzieci elementu all-accounts)
-        if(allAccounts != null){
-            for (var i = 0; i < allAccounts.children.length; i++) {
-                var description = allAccounts.children[i].querySelector(".account-description");
-                description.style.color = "rgba(255, 255, 255, 0.6)";
-                if(i%2 == 0){
-                    allAccounts.children[i].style.backgroundColor = "rgb(12, 6, 35)";
-                }
-                else{
-                    allAccounts.children[i].style.backgroundColor = "rgb(34, 15, 8)";
-                }
-                allAccounts.children[i].getElementsByClassName("account-link")[0].style.color = "white";
-            }
-        }
-
-        //WOJTEK
-        //transakcje
-        if(allTransactions != null){
-            for (var i = 0; i < allTransactions.length; i++) {
-                allTransactions[i].style.backgroundColor = "rgb(161, 161, 119)";
-            }
-        }
-
-        //przychody
-        if(allIncomes != null){
-            for (var i = 0; i < allIncomes.length; i++) {
-                // allIncomes[i].style.backgroundColor = "rgb(37, 73, 37)";
-                allIncomes[i].style.color = "rgb(37, 73, 37)";
-            }
-        }
-
-        //wydatki
-        if(allExpenses != null){
-            for (var i = 0; i < allExpenses.length; i++) {
-                // allExpenses[i].style.backgroundColor = "rgb(73, 37, 37)";
-                allExpenses[i].style.color = "rgb(73, 37, 37)";
-            }
-        }
-
-
-
-        content.style.backgroundColor = "#081132";
-
-        button.style.backgroundColor = "darkblue";
-        button.style.color = "white";
-        button.innerHTML = "Jasny motyw";
-
+        changeToDark();
         sessionStorage.setItem("darkmode", "true");
     } else {    //ustawianie jasnego motywu
         background.style.backgroundColor = "white";
