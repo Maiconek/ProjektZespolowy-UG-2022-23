3d905f7255b6e740d747d29101bdc33f20a21b1c
Marcin <mbaranowski57@gmail.com>
2023-01-18 14:07:40 +0100

    dodanie urli do resetu hasla

--- ManagerFinansow/ManagerFinansow/urls.py
+++ ManagerFinansow/ManagerFinansow/urls.py
@@ -26,7 +26,10 @@ urlpatterns = [
     path('', include('aplication.urls')),
     path('', include('UsersApp.urls')),
     
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset-password"),
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
     #path('api/', include('api.urls'))
 ]
 
