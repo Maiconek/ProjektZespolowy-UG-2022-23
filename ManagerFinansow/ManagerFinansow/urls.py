eb7c35acc97eeaf41edcabeed36cd2f15fa508ae
Marcin <mbaranowski57@gmail.com>
2023-01-18 13:55:54 +0100

    rozpoczecie pracy nad przywracaniem hasła

--- ManagerFinansow/ManagerFinansow/urls.py
+++ ManagerFinansow/ManagerFinansow/urls.py
@@ -17,14 +17,17 @@ from django.contrib import admin
 from django.urls import path, include
 from aplication.views import *
 from UsersApp.views import *
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     path('', include('aplication.urls')),
     path('', include('UsersApp.urls')),
+    
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset-password"),
     #path('api/', include('api.urls'))
-
 ]
 
 handler404 = 'aplication.views.error404'
