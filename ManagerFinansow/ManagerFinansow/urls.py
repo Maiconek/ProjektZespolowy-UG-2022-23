2ef5c4780855fc8f29bb9d54cdf0c4a96427cb1f
Marcin <mbaranowski57@gmail.com>
2023-01-18 14:24:54 +0100

    dodano formularz do resetu

--- ManagerFinansow/ManagerFinansow/urls.py
+++ ManagerFinansow/ManagerFinansow/urls.py
@@ -26,7 +26,8 @@ urlpatterns = [
     path('', include('aplication.urls')),
     path('', include('UsersApp.urls')),
     
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="registration/reset-password.html"), 
+    name="reset_password"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
