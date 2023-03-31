2b166360f6246b9a77af5b85730d7ad77a301301
caesarius00 <czarkowski15@gmail.com>
2023-03-31 02:21:18 +0200

    ciemny motyw - małe zmiany (animacje)

--- ManagerFinansow/aplication/static/scripts.js
+++ ManagerFinansow/aplication/static/scripts.js
@@ -1,8 +1,8 @@
-function changeToDark(){
+function changeToDark(isChange){
     var background = document.body;
     var button = document.getElementById("dark-mode-button");
     var content = document.getElementById("site-content");
-    var allAccounts = document.getElementById("all-accounts");
+    var allAccounts = document.getElementsByClassName("account");
     var allIncomes = document.getElementsByClassName("income");
     var allExpenses = document.getElementsByClassName("expense");
     var selects = document.getElementsByTagName("select");
@@ -20,126 +20,169 @@ function changeToDark(){
 
     lightBulb.src = "/static/images/bulb_on.png";
 
-    //WOJTEK
+
+
     var allTransactions = document.getElementsByClassName("transaction");
     var linkButton = document.getElementsByClassName("button");
 
-    var purple = document.getElementsByClassName("purple");
-    if(purple != null){
-        for(var i = 0; i < purple.length; i++){
-            purple[i].classList.remove("dark-mode");
-            purple[i].classList.add("dark-mode");
-        }
+    if(isChange){
+        $(background).addClass("dark-mode", 1000);
+        $(button).addClass("dark-mode", 1000);
+        $(content).addClass("dark-mode", 1000);
+        $(allAccounts).addClass("dark-mode", 1000);
+        $(allIncomes).addClass("dark-mode", 1000);
+        $(allExpenses).addClass("dark-mode", 1000);
+        $(selects).addClass("dark-mode", 1000);
+        $(inputs).addClass("dark-mode", 1000);
+        $(textMuted).addClass("dark-mode", 1000);
+        $(elements).addClass("dark-mode", 1000);
+        $(paginations).addClass("dark-mode", 1000);
+        $(formSelects).addClass("dark-mode", 1000);
+        $(chosenChoices).addClass("dark-mode", 1000);
+        $(chosenDrop).addClass("dark-mode", 1000);
+        $(categories).addClass("dark-mode", 1000);
+        $(allTransactions).addClass("dark-mode", 1000);
+        $(linkButton).addClass("dark-mode", 1000);
+    }else{
+        $(background).addClass("dark-mode");
+        $(button).addClass("dark-mode");
+        $(content).addClass("dark-mode");
+        $(allAccounts).addClass("dark-mode");
+        $(allIncomes).addClass("dark-mode");
+        $(allExpenses).addClass("dark-mode");
+        $(selects).addClass("dark-mode");
+        $(inputs).addClass("dark-mode");
+        $(textMuted).addClass("dark-mode");
+        $(elements).addClass("dark-mode");
+        $(paginations).addClass("dark-mode");
+        $(formSelects).addClass("dark-mode");
+        $(chosenChoices).addClass("dark-mode");
+        $(chosenDrop).addClass("dark-mode");
+        $(categories).addClass("dark-mode");
+        $(allTransactions).addClass("dark-mode");
+        $(linkButton).addClass("dark-mode");
     }
 
-    background.classList.remove("dark-mode");
-    background.classList.add("dark-mode");
 
-    content.classList.remove("dark-mode");
-    content.classList.add("dark-mode");
+
+    //WOJTEK
+
+    // var purple = document.getElementsByClassName("purple");
+    // if(purple != null){
+    //     for(var i = 0; i < purple.length; i++){
+    //         purple[i].classList.remove("dark-mode");
+    //         purple[i].classList.add("dark-mode");
+    //     }
+    // }
+
+    // background.classList.remove("dark-mode");
+    // background.classList.add("dark-mode");
+
+    // content.classList.remove("dark-mode");
+    // content.classList.add("dark-mode");
     
-    button.classList.remove("dark-mode");
-    button.classList.add("dark-mode");
+    // button.classList.remove("dark-mode");
+    // button.classList.add("dark-mode");
 
-    if(chosenDrop != null){
-        for(var i = 0; i < chosenDrop.length; i++){
-            chosenDrop[i].classList.remove("dark-mode");
-            chosenDrop[i].classList.add("dark-mode");
-        }
-    }
+    // if(chosenDrop != null){
+    //     for(var i = 0; i < chosenDrop.length; i++){
+    //         chosenDrop[i].classList.remove("dark-mode");
+    //         chosenDrop[i].classList.add("dark-mode");
+    //     }
+    // }
 
 
-    if(chosenChoices != null){
-        for(var i = 0; i < chosenChoices.length; i++){
-            chosenChoices[i].classList.remove("dark-mode");
-            chosenChoices[i].classList.add("dark-mode");
-        }
-    }
+    // if(chosenChoices != null){
+    //     for(var i = 0; i < chosenChoices.length; i++){
+    //         chosenChoices[i].classList.remove("dark-mode");
+    //         chosenChoices[i].classList.add("dark-mode");
+    //     }
+    // }
 
-    if(formSelects != null){
-        for(var i = 0; i < formSelects.length; i++){
-            formSelects[i].classList.remove("dark-mode");
-            formSelects[i].classList.add("dark-mode");
-        }
-    }
+    // if(formSelects != null){
+    //     for(var i = 0; i < formSelects.length; i++){
+    //         formSelects[i].classList.remove("dark-mode");
+    //         formSelects[i].classList.add("dark-mode");
+    //     }
+    // }
 
-    if(paginations != null){
-        for(var i = 0; i < paginations.length; i++){
-            paginations[i].classList.remove("dark-mode");
-            paginations[i].classList.add("dark-mode");
-        }
-    }
+    // if(paginations != null){
+    //     for(var i = 0; i < paginations.length; i++){
+    //         paginations[i].classList.remove("dark-mode");
+    //         paginations[i].classList.add("dark-mode");
+    //     }
+    // }
 
-    if(elements != null){
-        for(var i = 0; i < elements.length; i++){
-            elements[i].classList.remove("dark-mode");
-            elements[i].classList.add("dark-mode");
-        }
-    }
+    // if(elements != null){
+    //     for(var i = 0; i < elements.length; i++){
+    //         elements[i].classList.remove("dark-mode");
+    //         elements[i].classList.add("dark-mode");
+    //     }
+    // }
 
-    if(textMuted != null){
-        for(var i = 0; i < textMuted.length; i++){
-            textMuted[i].classList.remove("dark-mode");
-            textMuted[i].classList.add("dark-mode");
-        }
-    }
+    // if(textMuted != null){
+    //     for(var i = 0; i < textMuted.length; i++){
+    //         textMuted[i].classList.remove("dark-mode");
+    //         textMuted[i].classList.add("dark-mode");
+    //     }
+    // }
 
-    if(categories != null){
-        for(var i = 0; i < categories.length; i++){
-            categories[i].classList.remove("dark-mode");
-            categories[i].classList.add("dark-mode");
-        }
-    }
+    // if(categories != null){
+    //     for(var i = 0; i < categories.length; i++){
+    //         categories[i].classList.remove("dark-mode");
+    //         categories[i].classList.add("dark-mode");
+    //     }
+    // }
 
-    if(allAccounts != null){
-        for(var i = 0; i < allAccounts.children.length; i++){
-            allAccounts.children[i].classList.remove("dark-mode");
-            allAccounts.children[i].classList.add("dark-mode");
-        }
-    }
+    // if(allAccounts != null){
+    //     for(var i = 0; i < allAccounts.children.length; i++){
+    //         allAccounts.children[i].classList.remove("dark-mode");
+    //         allAccounts.children[i].classList.add("dark-mode");
+    //     }
+    // }
 
-    if(allTransactions != null){
-        for(var i = 0; i < allTransactions.length; i++){
-            allTransactions[i].classList.remove("dark-mode");
-            allTransactions[i].classList.add("dark-mode");
-        }
-    }
+    // if(allTransactions != null){
+    //     for(var i = 0; i < allTransactions.length; i++){
+    //         allTransactions[i].classList.remove("dark-mode");
+    //         allTransactions[i].classList.add("dark-mode");
+    //     }
+    // }
 
-    if(allIncomes != null){
-        for(var i = 0; i < allIncomes.length; i++){
-            allIncomes[i].classList.remove("dark-mode");
-            allIncomes[i].classList.add("dark-mode");
-        }
-    }
+    // if(allIncomes != null){
+    //     for(var i = 0; i < allIncomes.length; i++){
+    //         allIncomes[i].classList.remove("dark-mode");
+    //         allIncomes[i].classList.add("dark-mode");
+    //     }
+    // }
 
-    if(allExpenses != null){
-        for(var i = 0; i < allExpenses.length; i++){
-            allExpenses[i].classList.remove("dark-mode");
-            allExpenses[i].classList.add("dark-mode");
-        }
-    }
+    // if(allExpenses != null){
+    //     for(var i = 0; i < allExpenses.length; i++){
+    //         allExpenses[i].classList.remove("dark-mode");
+    //         allExpenses[i].classList.add("dark-mode");
+    //     }
+    // }
 
-    if(selects != null){
-        for(var i = 0; i < selects.length; i++){
-            selects[i].classList.remove("dark-mode");
-            selects[i].classList.add("dark-mode");
-        }
-    }
+    // if(selects != null){
+    //     for(var i = 0; i < selects.length; i++){
+    //         selects[i].classList.remove("dark-mode");
+    //         selects[i].classList.add("dark-mode");
+    //     }
+    // }
 
-    if(inputs != null){
-        for(var i = 0; i < inputs.length; i++){
-            inputs[i].classList.remove("dark-mode");
-            inputs[i].classList.add("dark-mode");
-        }
-    }
-
+    // if(inputs != null){
+    //     for(var i = 0; i < inputs.length; i++){
+    //         inputs[i].classList.remove("dark-mode");
+    //         inputs[i].classList.add("dark-mode");
+    //     }
+    // }
+    // }
 }
 
 function changeToLight(){
     var background = document.body;
     var button = document.getElementById("dark-mode-button");
     var content = document.getElementById("site-content");
-    var allAccounts = document.getElementById("all-accounts");
+    var allAccounts = document.getElementsByClassName("account");
     var allIncomes = document.getElementsByClassName("income");
     var allExpenses = document.getElementsByClassName("expense");
     var selects = document.getElementsByTagName("select");
@@ -162,102 +205,122 @@ function changeToLight(){
     var allTransactions = document.getElementsByClassName("transaction");
     var linkButton = document.getElementsByClassName("button");
 
-    var purple = document.getElementsByClassName("purple");
-    if(purple != null){
-        for(var i = 0; i < purple.length; i++){
-            purple[i].classList.remove("dark-mode");
-        }
-    }
 
-    background.classList.remove("dark-mode");
+    $(background).removeClass("dark-mode", 1000);
+    $(button).removeClass("dark-mode", 1000);
+    $(content).removeClass("dark-mode", 1000);
+    $(allAccounts).removeClass("dark-mode", 1000);
+    $(allIncomes).removeClass("dark-mode", 1000);
+    $(allExpenses).removeClass("dark-mode", 1000);
+    $(selects).removeClass("dark-mode", 1000);
+    $(inputs).removeClass("dark-mode", 1000);
+    $(paginations).removeClass("dark-mode", 1000);
+    $(formSelects).removeClass("dark-mode", 1000);
+    $(chosenChoices).removeClass("dark-mode", 1000);
+    $(chosenDrop).removeClass("dark-mode", 1000);
+    $(elements).removeClass("dark-mode", 1000);
+    $(textMuted).removeClass("dark-mode", 1000);
+    $(categories).removeClass("dark-mode", 1000);
+    $(allTransactions).removeClass("dark-mode", 1000);
+    $(linkButton).removeClass("dark-mode", 1000);
 
-    content.classList.remove("dark-mode");
+
+    // var purple = document.getElementsByClassName("purple");
+    // if(purple != null){
+    //     for(var i = 0; i < purple.length; i++){
+    //         purple[i].classList.remove("dark-mode");
+    //     }
+    // }
+
+    // background.classList.remove("dark-mode");
+
+    // content.classList.remove("dark-mode");
     
-    button.classList.remove("dark-mode");
+    // button.classList.remove("dark-mode");
 
-    if(chosenDrop != null){
-        for(var i = 0; i < chosenDrop.length; i++){
-            chosenDrop[i].classList.remove("dark-mode");
-        }
-    }
+    // if(chosenDrop != null){
+    //     for(var i = 0; i < chosenDrop.length; i++){
+    //         chosenDrop[i].classList.remove("dark-mode");
+    //     }
+    // }
 
-    if(chosenChoices != null){
-        for(var i = 0; i < chosenChoices.length; i++){
-            chosenChoices[i].classList.remove("dark-mode");
-        }
-    }
+    // if(chosenChoices != null){
+    //     for(var i = 0; i < chosenChoices.length; i++){
+    //         chosenChoices[i].classList.remove("dark-mode");
+    //     }
+    // }
 
-    if(formSelects != null){
-        for(var i = 0; i < formSelects.length; i++){
-            formSelects[i].classList.remove("dark-mode");
-        }
-    }
+    // if(formSelects != null){
+    //     for(var i = 0; i < formSelects.length; i++){
+    //         formSelects[i].classList.remove("dark-mode");
+    //     }
+    // }
 
-    if(paginations != null){
-        for(var i = 0; i < paginations.length; i++){
-            paginations[i].classList.remove("dark-mode");
-        }
-    }
+    // if(paginations != null){
+    //     for(var i = 0; i < paginations.length; i++){
+    //         paginations[i].classList.remove("dark-mode");
+    //     }
+    // }
 
-    if(elements != null){
-        for(var i = 0; i < elements.length; i++){
-            elements[i].classList.remove("dark-mode");
-        }
-    }
+    // if(elements != null){
+    //     for(var i = 0; i < elements.length; i++){
+    //         elements[i].classList.remove("dark-mode");
+    //     }
+    // }
 
-    if(textMuted != null){
-        for(var i = 0; i < textMuted.length; i++){
-            textMuted[i].classList.remove("dark-mode");
-        }
-    }
+    // if(textMuted != null){
+    //     for(var i = 0; i < textMuted.length; i++){
+    //         textMuted[i].classList.remove("dark-mode");
+    //     }
+    // }
 
-    if(categories != null){
-        for(var i = 0; i < categories.length; i++){
-            categories[i].classList.remove("dark-mode");
-        }
-    }
+    // if(categories != null){
+    //     for(var i = 0; i < categories.length; i++){
+    //         categories[i].classList.remove("dark-mode");
+    //     }
+    // }
 
-    if(allAccounts != null){
-        for(var i = 0; i < allAccounts.children.length; i++){
-            allAccounts.children[i].classList.remove("dark-mode");
-        }
-    }
+    // if(allAccounts != null){
+    //     for(var i = 0; i < allAccounts.children.length; i++){
+    //         allAccounts.children[i].classList.remove("dark-mode");
+    //     }
+    // }
 
-    if(allTransactions != null){
-        for(var i = 0 ; i < allTransactions.length; i++){
-            allTransactions[i].classList.remove("dark-mode");
-        }
-    }
+    // if(allTransactions != null){
+    //     for(var i = 0 ; i < allTransactions.length; i++){
+    //         allTransactions[i].classList.remove("dark-mode");
+    //     }
+    // }
 
-    if(allIncomes != null){
-        for(var i = 0; i < allIncomes.length; i++){
-            allIncomes[i].classList.remove("dark-mode");
-        }
-    }
+    // if(allIncomes != null){
+    //     for(var i = 0; i < allIncomes.length; i++){
+    //         allIncomes[i].classList.remove("dark-mode");
+    //     }
+    // }
 
-    if(allExpenses != null){
-        for(var i = 0; i < allExpenses.length; i++){
-            allExpenses[i].classList.remove("dark-mode");
-        }
-    }
+    // if(allExpenses != null){
+    //     for(var i = 0; i < allExpenses.length; i++){
+    //         allExpenses[i].classList.remove("dark-mode");
+    //     }
+    // }
 
-    if(selects != null){
-        for(var i = 0; i < selects.length; i++){
-            selects[i].classList.remove("dark-mode");
-        }
-    }
+    // if(selects != null){
+    //     for(var i = 0; i < selects.length; i++){
+    //         selects[i].classList.remove("dark-mode");
+    //     }
+    // }
 
-    if(inputs != null){
-        for(var i = 0; i < inputs.length; i++){
-            inputs[i].classList.remove("dark-mode");
-        }
-    }
+    // if(inputs != null){
+    //     for(var i = 0; i < inputs.length; i++){
+    //         inputs[i].classList.remove("dark-mode");
+    //     }
+    // }
 
 }
 
 function checkColorMode(){
     if(sessionStorage.getItem("darkmode") == "true"){
-        changeToDark();
+        changeToDark(false);
     }
 
 }
@@ -277,11 +340,14 @@ function darkMode() {
 
     //ustawianie ciemnego motywu
     if (sessionStorage.getItem("darkmode") == "false" || sessionStorage.getItem("darkmode") == null) {
-        changeToDark();
+        //animate backgroundusing jquery
+        $(background).animate({backgroundColor: "#0b0f12"}, 1000);
+        changeToDark(true);
         sessionStorage.setItem("darkmode", "true");
     } else {    //ustawianie jasnego motywu
-
-        changeToLight();
+        //animate backgroundusing jquery
+        $(background).animate({backgroundColor: "white"}, 1000);
+        changeToLight(true);
 
         lightBulb.src = "/static/images/bulb_off.png";
 
